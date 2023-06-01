4eaf69e5dedd892ae6127892d34f9ecadeb159df
Alexandre da Silva Estevao <alexandre.da.estevao@accenture.com>
2023-06-01 14:48:45 -0300

    Hystrix fault tolerance

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -3,7 +3,6 @@ package com.devsuperior.hrworker.resources;
 import java.util.List;
 
 import org.hibernate.annotations.common.util.impl.LoggerFactory;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -35,6 +34,14 @@ public class WorkerResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		
+		/*
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		*/
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
