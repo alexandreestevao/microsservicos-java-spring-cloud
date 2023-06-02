781006aefdbad411fdc2d4660581db01a1b35807
Alexandre da Silva Estevao <alexandre.da.estevao@accenture.com>
2023-06-02 16:10:33 -0300

    Configuration hr-config-server project

---
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -0,0 +1,22 @@
+package com.devsuperior.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class HrConfigServerApplication {
+
+//	@Value("${spring.cloud.config.server.git.username}")
+//	private String username;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+//	@Override
+//	public void run(String... args) throws Exception {
+//		//System.out.println("USERNAME = " + username);
+//	}
+}
